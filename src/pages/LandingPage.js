51a95b21d5473813ecb731c0227a689539f6b7ea
ameliakusuma <ameliakusuma25@gmail.com>
2021-06-24 15:44:06 +0700

    menambah parts hero dan most picked

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 import Header from 'parts/Header'
+import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked'
+
+import landingPage from 'json/landingPage.json';
 
 export default class LandingPage extends Component {
+    constructor(props){
+        super(props);
+        this.refMostPicked = React.createRef();
+    }
     render() {
         return (
-        <>
+        <>  {/* passing props ke header */}
             <Header {...this.props}></Header> 
-            {/* passing props ke header */}
+            {/* mengambil json dari landingPage*/}
+            <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+            {/* ini kalau heronya dikasih refMostPicked jadi bisa scroll down ke mostpicknya kalau button show me more di klik */}
+            <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
         </>
         );
     }
